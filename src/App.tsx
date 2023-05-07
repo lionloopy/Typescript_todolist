7ad605d27092eb97751bbeb082300d68ca693915
yunseo <lionloopy@naver.com>
2023-05-07 21:30:54 +0900

    feat : 투두리스트 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styled, { createGlobalStyle } from "styled-components";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 import { TodosContextProvider } from "./context/TodosContext";
@@ -14,19 +13,3 @@ const App = () => {
 };
 
 export default App;
-
-const Rootdiv = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const GlobalStyle = createGlobalStyle`
-    body{
-    padding : 0;
-    margin : 0;
-    }
-`;
